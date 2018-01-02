7fcc670569c7d4e6463c10b97689bfb3f105dfa0
Myth <zhouran1@kingsoft.com>
2018-01-02 17:29:53 +0800

    update config

---
+++ src/entry.js
@@ -0,0 +1,4 @@
+import Index from './Index.vue'
+
+// create the app instance.
+new Vue(Vue.util.extend({ el: '#weex' }, Index))
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,71 @@
-require('webpack')
-require('weex-loader')
-
 var path = require('path')
+var webpack = require('webpack')
+function resolve (dir) {
+  return path.join(__dirname, '..', dir)
+}
 
-module.exports = {
-  entry: {
-    main: path.join(__dirname, 'src', 'Index.vue?entry=true')
-  },
-  output: {
-    path: 'dist',
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.vue(\?[^?]+)?$/,
-        loaders: ['weex-loader']
+var bannerPlugin = new webpack.BannerPlugin({
+  banner: '// { "framework": "Vue" }\n',
+  raw: true
+})
+
+function getBaseConfig () {
+  return {
+    entry: {
+      'index': path.resolve('src', 'entry.js')
+    },
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': resolve('src'),
+        'util': path.resolve(__dirname, './src/api/util'),
+        'strings': path.resolve(__dirname, './src/i18n'),
+        'config': path.resolve(__dirname, './src/config.js'),
+        'service': path.resolve(__dirname, './src/api/service.js'),
+        'components': path.resolve(__dirname, './src/components')
       }
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/
+        }, {
+          test: /\.vue(\?[^?]+)?$/
+        }
+      ]
+    },
+    plugins: [
+      // new webpack.optimize.UglifyJsPlugin({compress: { warnings: false }}),
+      bannerPlugin
     ]
   }
 }
+
+var webConfig = getBaseConfig()
+webConfig.output.filename = '[name].web.js'
+webConfig.module.rules[1].loader = 'vue-loader'
+/**
+ * important! should use postTransformNode to add $processStyle for
+ * inline style normalization.
+ */
+webConfig.module.rules[1].options = {
+  compilerModules: [
+    {
+      postTransformNode: el => {
+        el.staticStyle = `$processStyle(${el.staticStyle})`
+        el.styleBinding = `$processStyle(${el.styleBinding})`
+      }
+    }
+  ]
+}
+
+var nativeConfig = getBaseConfig()
+nativeConfig.output.filename = '[name].weex.js'
+nativeConfig.module.rules[1].loader = 'weex-loader'
+
+module.exports = [webConfig, nativeConfig]
